0e859094b2e477bc1f3186a2e6c7ddaa0734bdf1
leizeting <leiztsh@sinosoft.com.cn>
2019-03-13 10:14:06 +0800

    创建springcloud demo项目

---
+++ springcloudcliet/src/main/java/com/my/SpringcloudclietApplication.java
@@ -0,0 +1,34 @@
+package com.my;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class SpringcloudclietApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudclietApplication.class, args);
+	}
+
+
+	@Value("${server.port}")
+	private String port;
+
+	/**
+	 * 浏览器输入http://localhost:2222/hi
+	 * @param name
+	 * @return
+	 */
+	@RequestMapping("/hi")
+	public String test(@RequestParam(value = "name", defaultValue = "forezp") String name){
+		return "hi " + name + " ,i am from port:" + port;
+	}
+
+}
